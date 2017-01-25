56cfedae0535d341b3b9609f30c3302e10035c58
srkrkalyan <srkrkalyan@gmail.com>
2017-01-24 22:56:17 -0700

    Update handler /blog/signup

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ import jinja2
 import os
 from google.appengine.ext import db
 import time
+import hashlib
 
 # Creates a reference to templates dir
 template_dir = os.path.join(os.path.dirname(__file__),'templates')
@@ -32,6 +33,13 @@ class Blog(db.Model):
 	blog = db.TextProperty(required = True)
 	created = db.DateTimeProperty(auto_now_add = True, indexed=True)
 	blog_id = db.IntegerProperty(required=True)
+
+
+class User(db.Model):
+	user_id = db.IntegerProperty(required=True)
+	user_name=db.StringProperty(required=True)
+	password = db.StringProperty(required=True)
+	email=db.StringProperty()
 
 
 class Handler(webapp2.RequestHandler):
@@ -49,12 +57,52 @@ class Handler(webapp2.RequestHandler):
 		blog_id = 1000+row_count
 		return blog_id
 
+	def new_user_id(self,row_count):
+		user_id = 5000+row_count
+		return user_id
+
+	def hashme(self,password):
+		return hashlib.sha256(password).hexdigest()
+
 class BlogHandler(Handler):
     def get(self):
     	if (db.GqlQuery("select * from Blog").count()) > 0:
 	    	blogs = db.GqlQuery("select * from Blog order by created desc").fetch(limit=10)
 	    	self.render("blog_front.html",blogs=blogs)
 
+class SignUp(Handler):
+	def get(self):
+		# Setting a cookie in response header as soon as a request is received to sign up page
+		users_count = db.GqlQuery("select * from User").count()
+		user_id = self.new_user_id(users_count)
+		cookie_value=str(user_id)+'|'+str(self.hashme(str(user_id)))
+		self.response.headers.add_header('Set-Cookie','user_id=%s; path=/' % cookie_value)
+		self.render("sign_up.html")
+	def post(self,*args,**kw):
+		username = self.request.get("username")
+		password=self.request.get("password")
+		verify_password = self.request.get("verify")
+		email = self.request.get("email")
+		users_count = db.GqlQuery("select * from User").count()
+		user_id = self.new_user_id(users_count)
+		if username and password and (password == verify_password):
+			user_entry = User(user_name=username, password = self.hashme(password), user_id=user_id,email=email)
+			user_entry.put()
+			#time.sleep(2)
+			self.redirect('/blog/welcome')
+		elif username and (password != verify_password):
+			error = "Password mismatch !!! Please retry"
+			self.render("sign_up.html",username=username,password=password, verify_password=verify_password,error=error)
+		else:
+			error = "Both username and password required. Please give all required details"
+			self.render("sign_up.html",username=username,password=password, verify_password=verify_password,error=error)			
+
+class Welcome(Handler):
+	def get(self):
+		cookie_value = self.request.cookies.get('user_id')
+		user_id=cookie_value.split('|')[0]
+		username=db.GqlQuery("select * from User where user_id="+user_id).get().user_name
+		self.render("welcome.html",username=username)
 
 class NewPostHandler(Handler):
 	def get(self):
@@ -87,5 +135,9 @@ class PermaLink(Handler):
 		self.render("blog_details.html")
 
 app = webapp2.WSGIApplication([
-    ('/blog', BlogHandler),('/blog/newpost', NewPostHandler),('/blog/([0-9]+)', PermaLink)
+	('/blog/signup', SignUp),
+    ('/blog', BlogHandler),
+    ('/blog/newpost', NewPostHandler),
+    ('/blog/([0-9]+)', PermaLink),
+    ('/blog/welcome', Welcome)
 ], debug=True)
