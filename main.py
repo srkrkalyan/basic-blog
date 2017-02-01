ade01f61b96dbdcef725fdd1a96f8513b59e6f0d
srkrkalyan <srkrkalyan@gmail.com>
2017-01-31 22:56:58 -0700

    new feature: edit comments

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ class Comment(db.Model):
 	user_name=db.StringProperty(required=True)
 	comment=db.TextProperty(required=True)
 	created = db.DateTimeProperty(auto_now_add = True)
+	comment_id = db.IntegerProperty(required=True)
 
 class Like(db.Model):
 	user_id=db.IntegerProperty(required=True)
@@ -119,12 +120,18 @@ class Handler(webapp2.RequestHandler):
 		return comments+7000+1
 
 	def login_status(self):
-		cookie_value = self.request.cookies.get('user_id').split('|')[0]
+		cookie_value = self.request.cookies.get('user_id')
 		if cookie_value:
-			if cookie_value == self.get_current_user():
+			if cookie_value.split('|')[0] == self.get_current_user():
 				return True
 			else:
 				return False
+		else:
+			return False
+
+	def user_owns_comment(self,comment_id):
+		if db.GqlQuery("select * from Comment where comment_id ="+str(comment_id)).get().user_id == int(self.get_current_user()):
+			return True
 		else:
 			return False
 
@@ -361,7 +368,8 @@ class CommentBlog(Handler):
 		user_name = db.GqlQuery("select * from User where user_id="+ str(user_id)).get().user_name
 		comment=self.request.get('comment')
 		if comment:
-			comment_entry=Comment(blog_id=int(blog_id), user_id=int(user_id), user_name=str(user_name), comment=comment)
+			comment_entry=Comment(blog_id=int(blog_id), user_id=int(user_id), 
+				user_name=str(user_name), comment=comment,comment_id=comment_id)
 			comment_entry.put()
 			# Comment this before deployment
 			time.sleep(2)
@@ -370,6 +378,29 @@ class CommentBlog(Handler):
 			time.sleep(2)
 			self.redirect('/blog/'+str(blog_id)+'/commentblog')
 
+
+class EditComment(Handler):
+	def get(self,blog_id,comment_id):
+		user_id = self.get_current_user()
+		if self.user_owns_comment(comment_id):
+			blog_entry = db.GqlQuery("select * from Blog where blog_id ="+ blog_id).get()
+			comments = db.GqlQuery("select * from Comment where blog_id ="+ blog_id).fetch(limit=1000)
+			self.render("comment.html",blog=blog_entry,comments=comments,edit_comment=int(comment_id))
+		else:
+			error="You cant edit other's comments"
+			blog_entry = db.GqlQuery("select * from Blog where blog_id ="+ blog_id).get()
+			comments = db.GqlQuery("select * from Comment where blog_id ="+ blog_id).fetch(limit=1000)
+			self.render("comment.html",blog=blog_entry,comments=comments,edit_error=error)
+
+	def post(self,blog_id,comment_id):
+		updated_comment = self.request.get("editcomment")
+		comment_entry = db.GqlQuery("select * from Comment where comment_id ="+str(comment_id)).get()
+		comment_entry.comment = updated_comment
+		comment_entry.put()
+		time.sleep(2)
+		blog_entry = db.GqlQuery("select * from Blog where blog_id ="+ blog_id).get()
+		comments = db.GqlQuery("select * from Comment where blog_id ="+ blog_id).fetch(limit=1000)
+		self.render("comment.html",blog=blog_entry,comments=comments)
 
 app = webapp2.WSGIApplication([
 	('/blog/signup', SignUp),
@@ -384,4 +415,5 @@ app = webapp2.WSGIApplication([
     ('/blog/([0-9]+)/likeblog', LikeBlog),
     ('/blog/([0-9]+)/commentblog', CommentBlog),
     ('/blog/([0-9]+)/unlikeblog', UnLikeBlog),
+    ('/blog/([0-9]+)/([0-9]+)/editcomment', EditComment)
 ], debug=True)
